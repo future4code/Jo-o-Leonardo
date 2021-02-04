22058332e3663d3a6b99071d724dec74be2f6b8e
Cibertartar <jpleonardo1990@hotmail.com>
2021-02-04 17:40:05 -0300

    ...

--- semana9/aula1/labex/src/components/TripDetailsPage.js
+++ semana9/aula1/labex/src/components/TripDetailsPage.js
@@ -1,1 +1,39 @@
-import Router from "./routes/Router";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useProtectedPage } from "../hooks/useProtectedPage";
+
+export const GetTripDetailPage = () => {
+  const [trip, setTrip] = useState({});
+  useProtectedPage();
+
+  useEffect(() => {
+    getTripDetail();
+  }, []);
+
+  const getTripDetail = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labeX/joao-leonardo-epps/trip/dYQMafr821vsZby0xRkr",
+        {
+          headers: {
+            auth: localStorage.getItem("token")
+          }
+        }
+      )
+      .then((res) => {
+        setTrip(res.data.trip);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Viagens:</h1>
+      <h2>Nome: {trip.name}</h2>
+      <h3>Onde: {trip.planet}</h3>
+      <p>{trip.description}</p>
+    </div>
+  );
+};
